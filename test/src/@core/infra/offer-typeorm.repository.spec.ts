2f7f2950bdf7b55c5cad36db67f5e27a05dc5d6e
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-07-25 09:43:07 -0300

    test: refactor OfferTypeOrmRepository tests for improved readability and consistency

--- test/src/@core/infra/offer-typeorm.repository.spec.ts
+++ test/src/@core/infra/offer-typeorm.repository.spec.ts
@@ -1,4 +1,3 @@
-import exp from "constants";
 import { Offer } from "src/@core/domain/offer.entity";
 import { OfferRepository } from "src/@core/domain/offer.repository";
 import { OfferTypeOrmRepository } from "src/@core/infra/db/offer-typeorm.repository";
@@ -7,30 +6,30 @@ import { AppDataSource } from "src/orm-config";
 import { DataSource, Repository } from "typeorm";
 
 describe('Offer TypeORM Repository', () => {
-    let dataSource: DataSource;
-    let offerRepository: OfferRepository;
-    let ormRepo: Repository<OfferEntity>;
+  let dataSource: DataSource;
+  let offerRepository: OfferRepository;
+  let ormRepo: Repository<OfferEntity>;
 
-    beforeEach(async() => {
-      dataSource = AppDataSource;
+  beforeEach(async() => {
+    dataSource = AppDataSource;
 
-      await dataSource.initialize();
-      ormRepo = dataSource.getRepository(OfferEntity);
-      offerRepository = new OfferTypeOrmRepository(ormRepo);
-    });
+    await dataSource.initialize();
+    ormRepo = dataSource.getRepository(OfferEntity);
+    offerRepository = new OfferTypeOrmRepository(ormRepo);
+  });
 
-    it('should insert an offer', async () => {
-        const offer = new Offer(1, 10, true, new Date(), new Date('09-20-2029'), new Date());
-        
-        await offerRepository.insert(offer);
+  it('should insert an offer', async () => {
+      const offer = new Offer(1, 10, true, new Date(), new Date('09-20-2029'), new Date());
+      
+      await offerRepository.insert(offer);
 
-        const model = await ormRepo.findOneBy({ id: offer.id });
-        
-        jest.spyOn(ormRepo, 'create');
-        jest.spyOn(ormRepo, 'save');
-        expect(model).toBeDefined();
-        expect(model.id).toBe(1);
-        expect(model.discountPercentage).toBe("10");
-        expect(model.enabled).toBe(true);
-    });
+      const model = await ormRepo.findOneBy({ id: offer.id });
+      
+      jest.spyOn(ormRepo, 'create');
+      jest.spyOn(ormRepo, 'save');
+      expect(model).toBeDefined();
+      expect(model.id).toBe(1);
+      expect(model.discountPercentage).toBe("10");
+      expect(model.enabled).toBe(true);
+  });
 });
