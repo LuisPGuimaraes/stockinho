1d6a14330b6bf0e6e263c66519fadcdf28d483d0
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-07-25 00:16:15 -0300

    feat: add MySQL configuration and update TypeORM setup for PostgreSQL

---
+++ config.js
@@ -0,0 +1,11 @@
+export const config = {
+    database: {
+      type: 'mysql', // ou 'postgres'
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 3306),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      poolSize: Number(process.env.DB_POOL_SIZE || 10),
+    },
+  }
--- src/orm-config.ts
+++ src/orm-config.ts
@@ -1,12 +1,13 @@
-import { DataSourceOptions } from "typeorm";
+import { DataSource } from 'typeorm';
+import { OfferEntity } from './@core/infra/db/offer.entity';
 
-export const ormConfig: DataSourceOptions = {
-    type: 'postgres',
-    host: 'postgres',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'mini_indexer',
-    synchronize: true,
-    entities: ['src/modules/kafka/entities/*.ts'],
-}
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'postgres',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'mini_indexer',
+  synchronize: true,
+  entities: [OfferEntity],
+});
