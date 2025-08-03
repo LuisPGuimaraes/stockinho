e575f14bb6bdc95d7447ce0fcf0c230af49fb7f5
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-08-03 02:44:23 -0300

    feat: add kafka configuration for offer creation events

--- config.js
+++ config.js
@@ -1,11 +1,20 @@
 export const config = {
-    database: {
-      type: 'mysql', // ou 'postgres'
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT || 3306),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      poolSize: Number(process.env.DB_POOL_SIZE || 10),
-    },
-  }
+	database: {
+		type: 'mysql', // ou 'postgres'
+		host: process.env.DB_HOST,
+		port: Number(process.env.DB_PORT || 3306),
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		poolSize: Number(process.env.DB_POOL_SIZE || 10),
+	},
+	kafka: {
+		topics: [
+			{
+				name: 'command.offers.create',
+				eventListener: 'OfferCreateListener',
+				consumerGroup: 'offer-create-group',
+			}
+		]
+	}
+}
