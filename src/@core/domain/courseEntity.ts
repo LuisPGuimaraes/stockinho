353087284b777f73457aa8c23391900b19ce6a41
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-08-05 22:58:50 -0300

    feat: refactor CourseEntity constructor to include properties and enable flag

--- src/@core/domain/courseEntity.ts
+++ src/@core/domain/courseEntity.ts
@@ -1,11 +1,24 @@
 export class CourseEntity {
-  constructor(
-    public id: number,
-    public name: string,
-    public level: string,
-    public kind: string,
-    public shift: string,
-    public createdAt: Date,
-    public updatedAt: Date
-  ) {}
+	id: number;
+	name: string;
+	level: string;
+	kind: string;
+	shift: string;
+	createdAt: Date;
+	updatedAt: Date;
+	enabled: boolean | null;
+
+	constructor(name: string, level: string, kind: string, shift: string, createdAt: Date, updatedAt: Date) {
+		this.name = name;
+		this.level = level;
+		this.kind = kind;
+		this.shift = shift;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.enabled = true;
+	}
+
+	disable(): void {
+		this.enabled = false;
+	}
 }
